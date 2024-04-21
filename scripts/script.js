7a867448919c45bceb0cd01e761e6a58f9a4e2f0
Isaac Martins <111926496+DevZank@users.noreply.github.com>
2024-04-21 02:29:20 -0300

    Subindo o Projeto

---
+++ scripts/script.js
@@ -0,0 +1,224 @@
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.getElementById("play-btn");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+let wave = document.getElementById("wave");
+let randomIcon = document.querySelector(".fa-random");
+let curr_track = document.createElement("audio");
+
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
+
+const music_list = [
+  {
+    img: "assets/images/Enemy.png",
+    name: "Enemy",
+    artist: "IMAGINE DRAGONS",
+    music: "assets/audios/Enemy.mp3",
+  },
+  {
+    img: "assets/images/NeverGonnaGiveYouUp.png",
+    name: "Never Gonna Give You Up",
+    artist: "Rick Astley",
+    music: "assets/audios/NeverGonnaGiveYouUp.mp3",
+  },
+  {
+    img: "assets/images/ᐳone.png",
+    name: "ᐳone",
+    artist: "VALORANT",
+    music: "assets/audios/ᐳone.mp3",
+  },
+  {
+    img: "assets/images/2WORLDS.png",
+    name: "2WORLDS",
+    artist: "VALORANT",
+    music: "assets/audios/2WORLDS.mp3",
+  },
+  {
+    img: "assets/images/Go!.png",
+    name: "Go!",
+    artist: "NEFFEX",
+    music: "assets/audios/Go!.mp3",
+  },
+];
+
+loadTrack(track_index);
+
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  reset();
+
+  curr_track.src = music_list[track_index].music;
+  curr_track.load();
+
+  track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+  track_name.textContent = music_list[track_index].name;
+  track_artist.textContent = music_list[track_index].artist;
+
+  updateTimer = setInterval(setUpdate, 1000);
+
+  curr_track.addEventListener("ended", nextTrack);
+  // random_bg_color();
+}
+
+function random_bg_color() {
+  let hex = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+  ];
+  let a;
+
+  function populate(a) {
+    for (let i = 0; i < 6; i++) {
+      let x = Math.round(Math.random() * 14);
+      let y = hex[x];
+      a += y;
+    }
+    return a;
+  }
+
+  let Color1 = populate("#");
+  let Color2 = populate("#");
+  var angle = "to right";
+
+  let gradient =
+    "linear-gradient(" + angle + "," + Color1 + ", " + Color2 + ")";
+  document.body.style.background = gradient;
+}
+
+function reset() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+
+function randomTrack() {
+  isRandom ? pauseRandom() : playRandom();
+}
+
+function playRandom() {
+  isRandom = true;
+  randomIcon.classList.add("randomActive");
+}
+
+function pauseRandom() {
+  isRandom = false;
+  randomIcon.classList.remove("randomActive");
+}
+
+function repeatTrack() {
+  let current_index = track_index;
+  loadTrack(current_index);
+  playTrack();
+}
+
+function playpauseTrack() {
+  isPlaying ? pauseTrack() : playTrack();
+}
+
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  playpause_btn.classList.remove("fa");
+  playpause_btn.classList.remove("fa-play-circle");
+  playpause_btn.classList.add("fa-solid");
+  playpause_btn.classList.add("fa-circle-pause");
+}
+
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  playpause_btn.classList.add("fa");
+  playpause_btn.classList.add("fa-play-circle");
+  playpause_btn.classList.remove("fa-solid");
+  playpause_btn.classList.remove("fa-circle-pause");
+}
+
+function nextTrack() {
+  if (track_index < music_list.length - 1 && isRandom === false) {
+    track_index += 1;
+  } else if (track_index < music_list.length - 1 && isRandom === true) {
+    let random_index = Number.parseInt(Math.random() * music_list.length);
+    track_index = random_index;
+  } else {
+    track_index = 0;
+  }
+  loadTrack(track_index);
+  playTrack();
+}
+
+function prevTrack() {
+  if (track_index > 0) {
+    track_index -= 1;
+  } else {
+    track_index = music_list.length - 1;
+  }
+  loadTrack(track_index);
+  playTrack();
+}
+
+function seekTo() {
+  let seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+function setUpdate() {
+  let seekPosition = 0;
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(
+      curr_track.currentTime - currentMinutes * 60
+    );
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(
+      curr_track.duration - durationMinutes * 60
+    );
+
+    if (currentSeconds < 10) {
+      currentSeconds = "0" + currentSeconds;
+    }
+    if (durationSeconds < 10) {
+      durationSeconds = "0" + durationSeconds;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = "0" + currentMinutes;
+    }
+    if (durationMinutes < 10) {
+      durationMinutes = "0" + durationMinutes;
+    }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
